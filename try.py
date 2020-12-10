a7db5710e80dde770ddbcef908a1de48c915b1d9
Shauray <shauray9@gmail.com>
2020-12-10 16:23:42 +0530

    did basic stuff with python chess

---
+++ try.py
@@ -0,0 +1,39 @@
+import chess
+board = chess.Board()
+
+moves = []
+
+readfile = open("moves.txt", "r")
+
+for line in readfile:
+    Type = line.split(" ")
+   
+for i in range(0, len(Type)):
+	a, b = Type[i].split(".")
+	# a, b = b.split("/")
+	moves.append(b)
+i = 0
+for move in moves:
+	i += 1
+	print("move number: ",i)
+	board.push_san(move)
+	print(board)
+	print("###############")
+
+
+print("stalemate: ",board.is_stalemate())
+
+print("insufficient_material: ",board.is_insufficient_material())
+
+print("gameover: ",board.is_game_over())
+
+print(board.legal_moves)
+# print(board)
+# print(board.legal_moves)
+
+# board.push_san("e4")
+# board.push_san("e5")
+# board.push_san("Qf3")
+# print(board)
+# print(board.legal_moves)
+# print(board.is_checkmate())
